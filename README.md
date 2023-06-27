71b67a046938e6a013e46baada022a84efc62818
cbot918 <cbot918@gmail.com>
2023-06-27 15:42:25 +0800

    add go-concurrency

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Blog
 
 [blog/go-tutor](https://github.com/cbot918/youtube/tree/blog/go-tutor)
+
+[blog/go-concurrency](https://github.com/cbot918/youtube/tree/blog/go-concurrency)
---
+++ go-concurrency/assets/upload_cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary')
+
+cloudinary.config({ 
+  cloud_name: 'yalecloud', 
+  api_key: '119372989697243', 
+  api_secret: 'tfwtQFL_JGO5_J70_0ABJkpxGHo' 
+});
+
+cloudinary.v2.uploader.upload("./2.png",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
